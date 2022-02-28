3088f0f0d90101d6713fb015bc10b5c6786f036f
muzammil-85 <muzukondeth85@gamil.com>
2022-02-28 09:49:59 +0530

    stable with least feature

---
+++ android/app/src/main/kotlin/com/example/dairy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dairy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:dairy/screens/note_view_screen.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:provider/provider.dart';
+
+import 'helper/note_provider.dart';
+import 'screens/note_edit_screen.dart';
+import 'screens/note_list_screen.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: NoteProvider(),
+      child: MaterialApp(
+        title: "Flutter Notes",
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => NoteListScreen(),
+          NoteViewScreen.route: (context) => NoteViewScreen(),
+          NoteEditScreen.route: (context) => NoteEditScreen(),
+        },
+      ),
+    );
+  }
+}
